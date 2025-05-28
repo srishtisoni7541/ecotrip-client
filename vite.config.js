04667a1a79c26457bd67af5e11118be6b00791b5
srishti soni <srishtisoni7541@gmail.com>
2025-05-28 12:44:39 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  build: {
-    rollupOptions: {
-      // Force use of JS version, not native
-      external: ['@rollup/rollup-linux-x64-gnu']
-    }
-  }
-})
+  plugins: [react(), tailwindcss()]})
+
